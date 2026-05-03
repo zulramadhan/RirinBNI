7dd082985528a5db18cff11958905625b0e760da
Dann <dann@gmail.com>
2026-05-03 12:56:18 -0400

    fix: remove wrong settings.py, update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,12 @@
-from ririnBNI.BNI_ririn.wsgi import application
+import os
+import sys
 
+# Tambahin path biar Django ketemu
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ririnBNI.BNI_ririn.settings')
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
 app = application
-
